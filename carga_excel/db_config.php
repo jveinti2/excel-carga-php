52b403703b91b240b7ff127c841c7f695b1054e4
Jhonatan S. Becerra <72992939+jveinti2@users.noreply.github.com>
2022-08-13 12:45:12 -0500

    Add files via upload

---
+++ carga_excel/db_config.php
@@ -0,0 +1,7 @@
+<?php
+	$dbHost = "10.1.1.195";
+	$dbDatabase = "db_factibilidadb2b";
+	$dbPasswrod = "IpBA+6yZ*pcOWJlhicJsGE";
+	$dbUser = "App_factibilidadb2b";
+	$mysqli = new mysqli($dbHost, $dbUser, $dbPasswrod, $dbDatabase);
+?>
